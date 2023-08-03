5556096b06adedb9366249f3407649a89fa84da9
João Nascimento <joaonascimento@MacBook-Air-de-Joao.local>
2023-08-03 13:11:38 -0300

    Formularios reativos de login e cadastro

--- src/app/modules/authentication/pages/home/home.component.ts
+++ src/app/modules/authentication/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard: boolean = true;
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  sigupForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmitLoginSubmit(): void {
+    console.log('Dados de login enviados!', this.loginForm.value);
+  }
+
+  onSubmitSignupSubmit(): void {
+    console.log('Dados de cadastro enviados!', this.sigupForm.value);
+  }
 }
